f0d1d695e4eb4b387bf089900e0db3a0cddb27ed
Miguel Raposo <migpovrap@hotmail.com>
2023-10-20 17:59:18 +0100

    TAD pedra
    Operações básicas e funções de alto nível correspondentes a este TAD, implementadas.
    A representação escolhida ainda me oferece dúvidas, pode vir a ser melhorada.

--- FP2324P2.py
+++ FP2324P2.py
@@ -19,7 +19,7 @@ def obtem_lin(i):
 def eh_intersecao(arg):
     return type(arg) == tuple and obtem_col(arg) in colunas and obtem_lin(arg) in linhas
 
-def intersocoes_iguais(i1,i2):
+def intersecoes_iguais(i1,i2):
     if eh_intersecao(i1) and eh_intersecao(i2):
         return obtem_col(i1) == obtem_col(i2) and obtem_lin(i1) == obtem_lin(i2)
     return False
@@ -47,3 +47,43 @@ def obtem_intersecoes_adjacentes(i,l):
     
 def ordena_intersecoes(t):
     return tuple(sorted(t,key= lambda t: t[1]))
+
+
+#TAD pedra
+#Pedra [0,()] O tuplo vazio corresponde à interseção onde se localiza a pedra
+#0 --> pedra branca
+#1 --> pedra preta
+#2 --> pedra neutra
+
+def cria_pedra_branca():
+    return [0,()]
+def cria_pedra_preta():
+    return [1,()]
+def cria_pedra_neutra():
+    return[2,()]
+
+def eh_pedra(arg):
+    return type(arg) == list and arg[0] in (0,1,3) and len(arg) == 2
+def eh_pedra_branca(p):
+    return p[0] == 0
+def eh_pedra_preta(p):
+    return p[0] == 1
+
+def pedras_iguais(p1,p2):
+    if eh_pedra(p1) and eh_pedra(p2):
+        return p1[0] == p2[0]
+    return False
+
+def pedra_para_str(p):
+    if p[0] == 0:
+        return 'O'
+    if p[0] == 1:
+        return 'X'
+    if p[0] == 2:
+        return '.'
+    
+def eh_pedra_jogador(p):
+    if eh_pedra_branca(p) or eh_pedra_preta(p):
+        return True
+    return False
+
