2bd2c0302331cb175c6c312e200186a07e0a3bae
Miguel Raposo <migpovrap@hotmail.com>
2023-10-24 17:13:33 +0100

    TAD Goban

--- FP2324P2.py
+++ FP2324P2.py
@@ -1,9 +1,9 @@
-colunas = ('A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S')
-linhas = (1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19)
+COLUNAS = ('A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S')
+LINHAS = (1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19)
 
 #TAD intersecao
-def cria_intersecao(col,lin):
-    if not (col in colunas and lin in linhas):
+def cria_intersecao(col,lin): # Tem de ser alterado
+    if not (col in COLUNAS and lin in LINHAS):
         raise ValueError('cria_intersecao: argumentos invalidos')
     return (col,lin)
 
@@ -16,8 +16,8 @@ def obtem_lin(i):
     return i[1]
 
 
-def eh_intersecao(arg):
-    return type(arg) == tuple and obtem_col(arg) in colunas and obtem_lin(arg) in linhas
+def eh_intersecao(arg): # Alterar usando o construtor
+    return type(arg) == tuple and obtem_col(arg) in COLUNAS and obtem_lin(arg) in LINHAS
 
 def intersecoes_iguais(i1,i2):
     if eh_intersecao(i1) and eh_intersecao(i2):
@@ -29,61 +29,230 @@ def intersecao_para_str(i):
     return obtem_col(i)+str(obtem_lin(i))
 
 def str_para_intersecao(str):
-    return (str[0],int(str[1:])) 
+    return cria_intersecao(str[0],int(str[1:])) 
 
 #Funções de Alto nível que estão associadas a este TAD (intersecao)
 def obtem_intersecoes_adjacentes(i,l):
     #Ordem de leitura (left to right, bottom  to top)
     interadj = ()
-    if colunas[colunas.index(obtem_col(i))-1] in colunas[:colunas.index(obtem_col(l))]:
-        interadj += (colunas[colunas.index(obtem_col(i))-1],obtem_lin(i)),
-    if obtem_lin(i)-1 <= obtem_lin(l):
+    if COLUNAS[COLUNAS.index(obtem_col(i))-1] in COLUNAS[:COLUNAS.index(obtem_col(l))]:
+        interadj += (COLUNAS[COLUNAS.index(obtem_col(i))-1],obtem_lin(i)),
+    if 0 < obtem_lin(i)-1 <= obtem_lin(l):
         interadj += (obtem_col(i),obtem_lin(i)-1),
-    if colunas[colunas.index(obtem_col(i))+1] in colunas[:colunas.index(obtem_col(l))]:
-        interadj += (colunas[colunas.index(obtem_col(i))+1],obtem_lin(i)),
+    if COLUNAS[COLUNAS.index(obtem_col(i))+1] in COLUNAS[:COLUNAS.index(obtem_col(l))+1]:
+        interadj += (COLUNAS[COLUNAS.index(obtem_col(i))+1],obtem_lin(i)),
     if obtem_lin(i)+1 <= obtem_lin(l):
         interadj += (obtem_col(i),obtem_lin(i)+1),
     return interadj
     
 def ordena_intersecoes(t):
-    return tuple(sorted(t,key= lambda t: t[1]))
+    return tuple(sorted((sorted(t,key= lambda t: t[0])), key= lambda i: i[1]))
 
 
 #TAD pedra
 #Pedra [0,()] O tuplo vazio corresponde à interseção onde se localiza a pedra
-#0 --> pedra branca
-#1 --> pedra preta
-#2 --> pedra neutra
+#0 --> pedra neutro
+#1 --> pedra branco
+#2 --> pedra preto
 
 def cria_pedra_branca():
-    return [0,()]
+    return 1
 def cria_pedra_preta():
-    return [1,()]
+    return 2
 def cria_pedra_neutra():
-    return[2,()]
+    return 0
 
 def eh_pedra(arg):
-    return type(arg) == list and arg[0] in (0,1,3) and len(arg) == 2
+    return type(arg) == int and arg in (0,1,2)
 def eh_pedra_branca(p):
-    return p[0] == 0
+    return p == 1
 def eh_pedra_preta(p):
-    return p[0] == 1
+    return p== 2
 
 def pedras_iguais(p1,p2):
     if eh_pedra(p1) and eh_pedra(p2):
-        return p1[0] == p2[0]
+        return p1 == p2
     return False
 
 def pedra_para_str(p):
-    if p[0] == 0:
-        return 'O'
-    if p[0] == 1:
-        return 'X'
-    if p[0] == 2:
-        return '.'
-    
+    switch = {
+        0:'.',
+        1:'O',
+        2:'X'
+    }
+    return switch[p]
+
+#Funções de Alto nível que estão associadas a este TAD (pedra)
 def eh_pedra_jogador(p):
     if eh_pedra_branca(p) or eh_pedra_preta(p):
         return True
     return False
+
+
+
+#TAD goban
+#O tabuleiro vai ser representado por uma tuplo de listas ([],[],[])
+def cria_goban_vazio(n):
+    if n not in (9,13,19):
+        raise ValueError('cria_goban_vazio: argumentos invalido')
+    return tuple([0 for i in range(n)] for i1 in range(n))
+
+def cria_goban(n,ib,ip):
+    if n not in (9,13,19):
+        raise ValueError('cria_goban_vazio: argumentos invalido')
+    g = cria_goban_vazio(n)
+    for inter in ib:
+        if not eh_intersecao(inter):
+            raise ValueError('cria_goban: argumentos invalidos')
+        g[COLUNAS.index(obtem_col(inter))][obtem_lin(inter)-1] = cria_pedra_branca()
+    for inter in ip:
+        if not eh_intersecao(inter):
+            raise ValueError('cria_goban: argumentos invalidos')
+        g[COLUNAS.index(obtem_col(inter))][obtem_lin(inter)-1] = cria_pedra_preta()
+    return g
+
+def cria_copia_goban(t):
+    tcopy = []
+    for l in t:
+        tcopy += [l.copy()]
+    return tuple(tcopy)
+
+def obtem_ultima_intersecao(g):
+    return (COLUNAS[len(g)-1],len(g[-1]))
+
+def obtem_pedra(g,i):
+    return (g[COLUNAS.index(obtem_col(i))][obtem_lin(i)-1])
+
+
+def obtem_cadeia(g,i):
+    tipo = g[COLUNAS.index(obtem_col(i))][obtem_lin(i)-1]
+    igual, inter, cadeia= (i,), (), ()
+    while len(igual) > 0: 
+        testcoord = igual[-1] 
+        cadeia += (testcoord,)
+        interigualtemp = ()
+        for el in obtem_intersecoes_adjacentes(testcoord,obtem_ultima_intersecao(g)):
+            if el not in inter and g[COLUNAS.index(obtem_col(el))][obtem_lin(el)-1] == tipo:
+                interigualtemp += (el,)
+                inter += (el,)
+        igual = igual[:-1] + interigualtemp 
+    cadeiafinal = []
+    for el in cadeia:
+        if el not in cadeiafinal:
+            cadeiafinal += [el]
+    return tuple(ordena_intersecoes(cadeiafinal)) 
+
+def coloca_pedra(g,i,p):
+    g[COLUNAS.index(obtem_col(i))][obtem_lin(i)-1] = p
+    return g
+
+def remove_pedra(g,i,):
+    g[COLUNAS.index(obtem_col(i))][obtem_lin(i)-1] = 0
+    return g
+
+def remove_cadeia(g,t):
+    for inter in t:
+        g[COLUNAS.index(obtem_col(inter))][obtem_lin(inter)] = 0
+    return g
+
+def eh_goban(arg):
+    if type(arg) == tuple and type(arg[0]) == list:
+        if len(arg) in LINHAS and len(arg[0]) in LINHAS:
+            for col in range(len(arg)):
+                if len(arg[col]) == len(arg[col-1]):
+                    for el in arg[col]:
+                        if el in (0,1,2):
+                            return True
+    return False
+
+def eh_intersecao_valida(g,i):
+    if eh_goban(g) and eh_intersecao(i):
+        if COLUNAS.index(obtem_col(i)) in range(len(g)) and obtem_col(i)-1 in range(len(g[COLUNAS.index(obtem_col(i))])):
+            return True
+    return False
+
+def goban_iguais(g1,g2):
+    if eh_goban(g1) and eh_goban(g2) and len(g1) == len(g2):
+        for icol in range(g1):
+            for irow in range(icol):
+                if g1[icol][irow] == g2[icol][irow]:
+                    return True
+    return False
+
+
+def goban_para_str(g):
+    gobanstr = '  '
+    for icol in range(len(g)):
+        gobanstr += ' ' + COLUNAS[icol]
+    i = -1
+    gobanstr += '\n'
+    while i >= -len(g[0]):
+        gobanstr += ' ' + str(i+len(g[0])+1) + ' '
+        for col in g:
+            if col[i] == 0:
+                gobanstr += '.'+ ' '
+            elif col[i] == 1:
+                gobanstr += 'O' + ' '
+            else:
+                gobanstr += 'X' + ' '
+        gobanstr += ' ' + str(i+len(g[0])+1)
+        gobanstr += '\n'
+        i -= 1
+    gobanstr += '  '
+    for icol in range(len(g)):
+        gobanstr += ' ' + COLUNAS[icol]
+    return gobanstr
+
+#Funções de Alto nível que estão associadas a este TAD (goban)
+def obtem_territorios(g):
+    terr = ()
+    inter = ()
+    for icol in range(len(g)):
+        for irow in range(len(g[icol])):
+            if (COLUNAS[icol],irow+1) not in inter:
+                if g[icol][irow] == 0:
+                    nterr = obtem_cadeia(g,(COLUNAS[icol],irow+1))
+                    if nterr not in terr:
+                        terr += (nterr,)
+                        inter += nterr
+    
+    return terr       
+
+def obtem_adjacentes_diferentes(g,t):
+    adj = ()
+    for inter in t:
+        if eh_pedra_jogador(obtem_pedra(g,inter)):
+            for cord in obtem_intersecoes_adjacentes(inter, obtem_ultima_intersecao(g)):
+                if obtem_pedra(g,cord) == cria_pedra_neutra:
+                    if cord not in adj:
+                        adj+= (cord),
+        if not eh_pedra_jogador(obtem_pedra(g,inter)):
+            for cord in obtem_intersecoes_adjacentes(inter, obtem_ultima_intersecao(g)):
+                 if obtem_pedra(g,cord) == cria_pedra_branca or cria_pedra_preta:
+                    if cord not in adj:
+                        adj+= (cord),
+    return ordena_intersecoes(adj)
+
+def jogada(g,i,p):# Falta Verificar as liberdades e remover as pedras do jogador adversário caso necessário de acordo com as pedras
+    return coloca_pedra(g,i,p)
+
+
+def obtem_pedras_jogadores(g):
+    cadia_branco = ()
+    cadeia_preto =()
+    for icol in range(len(g)):
+        for irow in range(len(g[icol])):
+            for cord in obtem_cadeia(g,(COLUNAS[icol],irow+1)):
+                if eh_pedra_branca(obtem_pedra(g,cord)):
+                    if cord not in cadia_branco:
+                        cadia_branco += (cord),
+                elif eh_pedra_preta(obtem_pedra(g,cord)):
+                    if cord not in cadeia_preto:
+                        cadeia_preto += (cord),
+    return (len(cadia_branco),len(cadeia_preto))
+                    
+    
+
+
+
 
--- manualtest.py
+++ manualtest.py
@@ -13,6 +13,56 @@ import FP2324P2 as fp
 #print(tuple(fp.intersecao_para_str(i) for i in fp.obtem_intersecoes_adjacentes(i1,fp.cria_intersecao('S',19))))
 #print(fp.obtem_intersecoes_adjacentes(i1,('S',19)))
 
-tup = (fp.cria_intersecao('A',1),fp.cria_intersecao('A',3),fp.cria_intersecao('B',1),fp.cria_intersecao('B',2))
-print(fp.ordena_intersecoes(tup))
-print(tuple(fp.intersecao_para_str(i) for i in fp.ordena_intersecoes(tup)))
+#tup = (fp.cria_intersecao('A',1),fp.cria_intersecao('A',3),fp.cria_intersecao('B',1),fp.cria_intersecao('B',2))
+#print(fp.ordena_intersecoes(tup))
+#print(tuple(fp.intersecao_para_str(i) for i in fp.ordena_intersecoes(tup)))
+
+
+#tab[0][3] = 1
+#rint(tab)
+#print(len(tab))
+#print(len(tab[0]))
+
+#tabcopy = fp.cria_copia_goban(tab)
+#tabcopy[0][3] = 1
+#print(tab)
+#print(tabcopy)
+
+#print(fp.obtem_ultima_intersecao(tab))
+#print(fp.obtem_pedra(tab,('A',4)))
+#tab = fp.cria_goban_vazio(9)
+#tab[0][2] = 1
+#tab[1][2] = 1
+#tab[2][2] = 1
+#tab[2][1] = 1
+#tab[2][0] = 1
+#tab[3][3] = 1
+#tab[3][2] = 1
+#tab[3][1] = 1
+#tab[6][8] = 2
+#tab[6][7] = 2
+#tab[6][6] = 2
+#tab[7][6] = 2
+#tab[8][6] = 2
+#print(fp.obtem_cadeia(tab,('H',8)))
+#print(tab)
+#print(fp.obtem_cadeia(tab,('A',4)))
+#print(fp.goban_para_str(tab))
+#ter =fp.obtem_territorios(tab)
+#print(tuple(fp.intersecao_para_str(i) for i in fp.obtem_adjacentes_differentes(tab,ter[0])))
+#print(fp.obtem_pedras_jogadores(tab))
+ 
+g = fp.cria_goban_vazio(9)
+b, p = fp.cria_pedra_branca(), fp.cria_pedra_preta()
+ib = 'C1', 'C2', 'C3', 'D2', 'D3', 'D4', 'A3', 'B3'
+ip = 'E4', 'E5', 'F4', 'F5', 'G6', 'G7'
+for i in ib: fp.coloca_pedra(g, fp.str_para_intersecao(i), b)
+for i in ip: fp.coloca_pedra(g, fp.str_para_intersecao(i), p)
+cad = fp.obtem_cadeia(g, fp.cria_intersecao('F',5))
+
+liberdades = fp.obtem_adjacentes_diferentes(g, cad)
+print(cad)
+print(fp.goban_para_str(g))
+print(tuple(fp.intersecao_para_str(i) for i in liberdades))
+
+
